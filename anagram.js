a8c9c91ea0c9c3576c7f70a28c3a0763aff60d27
Vikas2064 <vikask200202@gmail.com>
2024-02-26 13:47:40 +0530

    second commit

--- anagram.js
+++ anagram.js
@@ -1,6 +1,17 @@
-function Anagram(let str1,let str2)
+function array_to_string(str1)
 {
-  ////write your code here
+   return Array.from(str1);
 }
 
-Anagram("hello", "elloh")
+function Anagram(str1,str2)
+{
+   if(array_to_string(str1)===array_to_string(str2))
+   {
+    return true;
+   }
+   return false;
+}
+
+
+let res=Anagram("hello", "elloh")
+console.log(res);
